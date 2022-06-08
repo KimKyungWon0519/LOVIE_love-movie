43d011245ee30f4ec798a3e0012ea60c5ed79106
Kyungwon Kim <kyungwon0519@polaris3d.co>
2022-06-08 23:31:50 +0900

    FEAT: Add checkError

--- lib/src/domain/repository/base_repository.dart
+++ lib/src/domain/repository/base_repository.dart
@@ -1,3 +1,5 @@
-abstract class BaseRepository {
+import 'package:lovie_lovemovie/src/data/models/response_model.dart';
 
+abstract class BaseRepository {
+  ResponseModel checkError(Function function);
 }
