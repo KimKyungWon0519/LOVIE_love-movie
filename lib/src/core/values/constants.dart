73012f33c6ef0703d6e75b40f2c77bfc39db5010
Kyungwon Kim <kyungwon0519@polaris3d.co>
2022-06-12 10:18:43 +0900

    FEAT: Add base url

---
+++ lib/src/core/values/constants.dart
@@ -0,0 +1,1 @@
+const String baseUrl = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest';
--- lib/src/data/data_sources/movie_api.dart
+++ lib/src/data/data_sources/movie_api.dart
@@ -1,9 +1,10 @@
 import 'package:dio/dio.dart';
+import 'package:lovie_lovemovie/src/core/values/constants.dart';
 import 'package:retrofit/http.dart';
 
 part 'movie_api.g.dart';
 
-@RestApi()
+@RestApi(baseUrl: baseUrl)
 abstract class MovieApi {
   factory MovieApi(Dio dio, {String baseUrl}) = _MovieApi;
 }
