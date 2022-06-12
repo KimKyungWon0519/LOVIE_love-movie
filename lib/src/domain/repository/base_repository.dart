36234bafea9daecaec74c3e33ea74148786e0f9f
Kyungwon Kim <kyungwon0519@polaris3d.co>
2022-06-12 09:55:25 +0900

    STYLE: Change package name - models to entities

--- lib/src/domain/repository/base_repository.dart
+++ lib/src/domain/repository/base_repository.dart
@@ -1,4 +1,4 @@
-import 'package:lovie_lovemovie/src/data/models/response_model.dart';
+import '../../data/entities/response_model.dart';
 
 abstract class BaseRepository {
   ResponseModel checkError(Function function);
