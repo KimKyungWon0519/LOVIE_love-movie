cadedde41541eaf4a328a5260bb64664c26b4d98
Kyungwon Kim <kyungwon0519@polaris3d.co>
2022-06-18 20:09:31 +0900

    FEAT: Return daily box office entity on getDailyBoxOffice function

--- lib/src/data/data_sources/movie_api.dart
+++ lib/src/data/data_sources/movie_api.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:lovie_lovemovie/src/core/values/configs.dart';
 import 'package:lovie_lovemovie/src/core/values/constants.dart';
+import 'package:lovie_lovemovie/src/data/entities/daily_box_office.dart';
 import 'package:retrofit/http.dart';
 
 part 'movie_api.g.dart';
@@ -10,7 +11,7 @@ abstract class MovieApi {
   factory MovieApi(Dio dio, {String baseUrl}) = _MovieApi;
 
   @GET('/boxoffice/searchDailyBoxOfficeList.json')
-  Future getDailyBoxOffice({
+  Future<DailyBoxOffice> getDailyBoxOffice({
     @Query('key') String key = apiKey,
     @Query('targetDt') required String targetDt,
   });
