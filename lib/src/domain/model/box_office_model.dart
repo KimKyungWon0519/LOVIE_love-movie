336863b8c8b941ab26faf64aa30a1265e2ff9ff9
Kyungwon Kim <kyungwon0519@polaris3d.co>
2022-06-20 22:37:57 +0900

    CREATE: Create box office model

---
+++ lib/src/domain/model/box_office_model.dart
@@ -0,0 +1,41 @@
+import 'package:json_annotation/json_annotation.dart';
+
+class BoxOfficeModel {
+  int rnum;
+  int rank;
+  int rankInten;
+  String rankOldAndNew;
+  String movieCd;
+  String movieNm;
+  String openDt;
+  int salesAmt;
+  double salesShare;
+  double salesChange;
+  int salesAcc;
+  int audiCnt;
+  int audiInten;
+  int audiChange;
+  int audiAcc;
+  int scrnCnt;
+  int showCnt;
+
+  BoxOfficeModel({
+    required this.rnum,
+    required this.rank,
+    required this.rankInten,
+    required this.rankOldAndNew,
+    required this.movieCd,
+    required this.movieNm,
+    required this.openDt,
+    required this.salesAmt,
+    required this.salesShare,
+    required this.salesChange,
+    required this.salesAcc,
+    required this.audiCnt,
+    required this.audiInten,
+    required this.audiChange,
+    required this.audiAcc,
+    required this.scrnCnt,
+    required this.showCnt,
+  });
+}
