20fb5027d2d79a133ee155535b0c95cb70179d0d
Kyungwon Kim <kyungwon0519@polaris3d.co>
2022-06-25 18:56:50 +0900

    CRAETE: Create box office use case

--- lib/src/domain/usecases/box_office_usecase.dart
+++ lib/src/domain/usecases/box_office_usecase.dart
@@ -1,3 +1,11 @@
-class BoxOfficeUseCase {
+import 'package:lovie_lovemovie/src/domain/repository/movie_repository.dart';
 
+class BoxOfficeUseCase {
+  final MovieRepository movieRepository;
+
+  BoxOfficeUseCase({required this.movieRepository});
+
+  dynamic getDailyBoxOffice(String date) async {
+    return await movieRepository.getDailyBoxOffice(date);
+  }
 }
