06f0e5b26257bf9b0335fb31a05fa27240cb50b3
Kyungwon Kim <kyungwon0519@polaris3d.co>
2022-07-05 00:04:08 +0900

    CREATE: Create home viewmodel

--- lib/dependency_injection.dart
+++ lib/dependency_injection.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:lovie_lovemovie/src/features/domain/repository/movie_repository.dart';
+import 'package:lovie_lovemovie/src/features/presentation/viewmodel/home_viewmodel.dart';
 
 import 'src/features/data/data_sources/movie_api.dart';
 import 'src/features/data/repository/movie_repository_impl.dart';
@@ -8,9 +10,10 @@ import 'src/features/domain/usecases/box_office_usecase.dart';
 final getIt = GetIt.instance;
 
 void setUp() {
-  getIt.registerLazySingleton<MovieApi>(() => MovieApi(Dio()));
+  MovieApi movieApi = MovieApi(Dio());
+  MovieRepository movieRepository = MovieRepositoryImpl(movieApi: movieApi);
+  BoxOfficeUseCase boxOfficeUseCase =
+      BoxOfficeUseCase(movieRepository: movieRepository);
   getIt.registerLazySingleton(
-      () => MovieRepositoryImpl(movieApi: getIt.get<MovieApi>()));
-  getIt.registerLazySingleton(() =>
-      BoxOfficeUseCase(movieRepository: getIt.get<MovieRepositoryImpl>()));
+      () => HomeViewModel(boxOfficeUseCase: boxOfficeUseCase));
 }
---
+++ lib/src/features/presentation/viewmodel/home_viewmodel.dart
@@ -0,0 +1,9 @@
+import 'package:lovie_lovemovie/src/features/domain/usecases/box_office_usecase.dart';
+
+class HomeViewModel {
+  final BoxOfficeUseCase boxOfficeUseCase;
+
+  const HomeViewModel({
+    required this.boxOfficeUseCase,
+  });
+}
