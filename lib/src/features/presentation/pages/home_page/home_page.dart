ae6888d149e6e4ed504983fc27b0f4dc3129d871
Kyungwon Kim <kyungwon0519@polaris3d.co>
2022-07-09 17:32:14 +0900

    FEAT: Show Daily Box Office Data

--- lib/src/features/presentation/pages/home_page/home_page.dart
+++ lib/src/features/presentation/pages/home_page/home_page.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lovie_lovemovie/dependency_injection.dart';
+import 'package:lovie_lovemovie/src/features/presentation/bloc/box_office_bloc.dart';
+import 'package:lovie_lovemovie/src/features/presentation/viewmodel/home_viewmodel.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('home page'),
+    HomeViewModel homeViewModel = getIt.get<HomeViewModel>();
+
+    homeViewModel.getDailyBoxOffice('20220601');
+
+    return BlocProvider(
+      create: (_) => homeViewModel.boxOfficeBloc,
+      child: BlocBuilder<BoxOfficeBloc, List>(
+        builder: (context, state) => Scaffold(
+          body: Center(
+            child: Text('$state'),
+          ),
+        ),
       ),
     );
   }
--- lib/src/features/presentation/viewmodel/home_viewmodel.dart
+++ lib/src/features/presentation/viewmodel/home_viewmodel.dart
@@ -1,9 +1,20 @@
+import 'package:lovie_lovemovie/src/features/domain/model/box_office_model.dart';
 import 'package:lovie_lovemovie/src/features/domain/usecases/box_office_usecase.dart';
+import 'package:lovie_lovemovie/src/features/presentation/bloc/box_office_bloc.dart';
 
-class HomeViewModel {
+class HomeViewModel extends Object {
   final BoxOfficeUseCase boxOfficeUseCase;
+  final BoxOfficeBloc boxOfficeBloc;
 
   const HomeViewModel({
     required this.boxOfficeUseCase,
+    required this.boxOfficeBloc,
   });
+
+  getDailyBoxOffice(String date) async {
+    List<BoxOfficeModel> boxOfficeList =
+        await boxOfficeUseCase.getDailyBoxOffice(date);
+
+    boxOfficeBloc.updateBoxOfficeBloc = boxOfficeList;
+  }
 }
