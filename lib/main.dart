f2c24ce44a5e6db69bd5b969d915de8ec331963b
Kyungwon Kim <kyungwon0519@polaris3d.co>
2022-07-09 17:33:55 +0900

    STYLE: Add ui dir

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lovie_lovemovie/src/features/presentation/ui/pages/home_page/home_page.dart';
 import 'dependency_injection.dart' as di;
-import 'src/features/presentation/pages/home_page/home_page.dart';
 
 void main() {
   di.setUp();
