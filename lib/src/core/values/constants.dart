508ab88916b168c8e9b96e940fd32a12aec09457
Kyungwon Kim <kyungwon0519@polaris3d.co>
2022-07-09 19:14:14 +0900

    FEAT: Add naver search api base url

--- lib/src/core/values/constants.dart
+++ lib/src/core/values/constants.dart
@@ -1,1 +1,4 @@
-const String baseUrl = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest';
+const String movieApiBaseUrl =
+    'http://www.kobis.or.kr/kobisopenapi/webservice/rest';
+const String naverSearchApiBaseUrl =
+    'https://openapi.naver.com/v1/search/movie.json';
