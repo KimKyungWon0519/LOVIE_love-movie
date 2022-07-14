15d3dd88be32257429dd5780174e1034bf31a190
Kyungwon Kim <kyungwon0519@polaris3d.co>
2022-07-14 12:40:51 +0900

    FEAT: Init Naver Api Classes

--- lib/dependency_injection.dart
+++ lib/dependency_injection.dart
@@ -1,6 +1,11 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:lovie_lovemovie/src/core/values/configs.dart';
+import 'package:lovie_lovemovie/src/features/data/data_sources/naver_search_api.dart';
+import 'package:lovie_lovemovie/src/features/data/repository/naver_repository_impl.dart';
 import 'package:lovie_lovemovie/src/features/domain/repository/movie_repository.dart';
+import 'package:lovie_lovemovie/src/features/domain/repository/naver_repository.dart';
+import 'package:lovie_lovemovie/src/features/domain/usecases/naver_api_usecase.dart';
 import 'package:lovie_lovemovie/src/features/presentation/bloc/box_office_bloc.dart';
 import 'package:lovie_lovemovie/src/features/presentation/viewmodel/home_viewmodel.dart';
 
@@ -16,8 +21,17 @@ void setUp() {
   BoxOfficeUseCase boxOfficeUseCase =
       BoxOfficeUseCase(movieRepository: movieRepository);
   BoxOfficeBloc boxOfficeBloc = BoxOfficeBloc([]);
+  NaverSearchApi naverSearchApi = NaverSearchApi(Dio(BaseOptions(headers: {
+    'X-Naver-Client-Id': naverClientID,
+    'X-Naver-Client-Secret': naverClientPW,
+  })));
+  NaverRepoistory naverRepoistory =
+      NaverRespositoryImpl(naverSearchApi: naverSearchApi);
+  NaverApiUseCase naverApiUseCase =
+      NaverApiUseCase(naverRepoistory: naverRepoistory);
   getIt.registerLazySingleton(() => HomeViewModel(
         boxOfficeUseCase: boxOfficeUseCase,
+        naverApiUseCase: naverApiUseCase,
         boxOfficeBloc: boxOfficeBloc,
       ));
 }
