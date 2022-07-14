9fa6a70006ae2b7c6a9bdeb336feaeea0cb381cf
Kyungwon Kim <kyungwon0519@polaris3d.co>
2022-07-14 12:41:28 +0900

    FEAT: Create naver search api

--- lib/src/features/data/data_sources/naver_search_api.dart
+++ lib/src/features/data/data_sources/naver_search_api.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:lovie_lovemovie/src/core/values/constants.dart';
+import 'package:lovie_lovemovie/src/features/data/entities/naver_move_result_entity.dart';
 import 'package:retrofit/http.dart';
 
 part 'naver_search_api.g.dart';
@@ -7,4 +8,9 @@ part 'naver_search_api.g.dart';
 @RestApi(baseUrl: naverSearchApiBaseUrl)
 abstract class NaverSearchApi {
   factory NaverSearchApi(Dio dio, {String baseUrl}) = _NaverSearchApi;
+
+  @GET('')
+  Future<NaverMovieResultEntity> getMovieImage({
+    @Query('query') required String name,
+  });
 }
